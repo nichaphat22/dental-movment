4f2abe3fc6daa45f81f35dfc9bdd6a9886a64f0f
nichaphat22 <nichaphat.si@kkumail.com>
2025-05-17 03:16:15 +0700

    Change server build f to b

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://dental-movment-production.up.railway.app',
     withCredentials: true,
 })
 
