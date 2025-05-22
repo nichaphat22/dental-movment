2a38bc944d9cac11e431a64b113975039769d299
nichaphat22 <nichaphat.si@kkumail.com>
2025-05-22 14:53:43 +0700

    change link URL

--- frontend/src/page/loginSuccess/Loginn.jsx
+++ frontend/src/page/loginSuccess/Loginn.jsx
@@ -202,7 +202,7 @@ const Loginn = () => {
               className="flex justify-center md:justify-start
                             "
             >
-              <a href="https://dental-movment-production.up.railway.app/api/auth/google">
+              <a href="https://dental-movmentofrpd.up.railway.app/api/auth/google">
                 <button>Login with Google</button>
               </a>
               <HiOutlineArrowLongRight
